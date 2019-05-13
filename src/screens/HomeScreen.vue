455d982ed15b6e3bc0ce7787cdca6c37e6172e26
yoshiji <yfbiology2@gmail.com>
2019-05-13 22:06:31 +0900

    vuex ミス

--- src/screens/HomeScreen.vue
+++ src/screens/HomeScreen.vue
@@ -1,16 +1,14 @@
 <template>
   <scroll-view>
     <nb-list>
-      <nb-list-item v-for="todo in todos">
-        <nb-text>{{ todo.title }}</nb-text>
+      <nb-list-item v-for="meetup in meetups">
+        <nb-text>{{ meetup.title }}</nb-text>
       </nb-list-item>
     </nb-list>
   </scroll-view>
 </template>
 
 <script>
-  import axios from 'axios';
-
   export default {
     props: {
       navigation: {
@@ -26,15 +24,19 @@
       // vuex-5. ミューテーションした後のデータが反映されている
       todos() {
         return this.$store.state.todos
+      },
+      meetups() {
+        console.log(this.$store.state.meetups)
+        return this.$store.state.meetups
       }
     },
     created() {
       // vuex-1.vuexのfetchTodosアクションを実行
       this.$store.dispatch('fetchTodos')
+      this.$store.dispatch('fetchMeetups')
     },
     methods: {
       goToScreen1() {
-        this.$store.
         this.navigation.navigate('ScreenOne')
       }
     }
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,18 @@
 import Vuex from 'vuex';
 import Vue from 'vue-native-core';
 import axios from 'axios';
+import {Platform} from 'react-native';
 
 Vue.use(Vuex);
+
+const BASE_URL = Platform.OS === 'ios' ? 'http://localhost:3001/api/v1'
+                                          : 'http://10.0.2.2:3001/api/v1';
 
 export default new Vuex.Store({
   // ステートとして保存したいデータ
   state: {
-    todos: []
+    todos: [],
+    meetups: [],
   },
   // store中のstateには、ゲッターを通して取得できる
   getters: {
@@ -16,7 +21,7 @@ export default new Vuex.Store({
   // コンポーネント中のメソッドのようなもの
   actions: {
     // vuex-2.homeScreenからのアクションディスパッチに対応
-    fetchTodos({ commit, todos }) {
+    fetchTodos({ commit, state }) {
       axios.get('https://jsonplaceholder.typicode.com/todos')
         .then(res => {
           const todos = res.data;
@@ -24,12 +29,24 @@ export default new Vuex.Store({
           commit('setTodos', todos);
           return state.todos;
         })
+    },
+    fetchMeetups({ commit, state }) {
+      axios.get(`${BASE_URL}/meetups`)
+        .then(res => {
+          const meetups = res.data;
+          commit('setMeetups', meetups);
+          return state.meetups;
+        })
     }
   },
   mutations: {
     setTodos(state, todos) {
       // vuex-4.stateのtodosに反映する
       Vue.set(state, 'todos', todos)
+    },
+    setMeetups(state, meetups) {
+      console.log(meetups.length + 'ほげ')
+      Vue.set(state, 'meetups', meetups);
     }
   }
 })
