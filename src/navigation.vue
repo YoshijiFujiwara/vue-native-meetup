d9fda4a40df5983a5eed55e8106d7e55a70291e9
yoshiji <yfbiology2@gmail.com>
2019-05-13 12:05:29 +0900

    navigation.vue

---
+++ src/navigation.vue
@@ -0,0 +1,42 @@
+<template>
+  <view class="container">
+    <AppNavigation />
+  </view>
+</template>
+
+<script>
+  import HomeScreen from '@/screens/HomeScreen';
+  import Screen1 from '@/screens/Screen1';
+  import Screen2 from '@/screens/Screen2';
+
+  import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+  const HomeStack = createStackNavigator(
+    {
+      Home: HomeScreen,
+      ScreenOne: Screen1,
+      ScreenTwo: Screen2
+    },
+    {
+      initialRouteName: 'Home'
+    }
+  );
+
+  const AppNavigation = createAppContainer(HomeStack)
+
+  export default {
+    components: {
+      HomeScreen, AppNavigation
+    }
+  }
+</script>
+
+<style>
+  .container {
+    background-color: lightblue;
+    flex: 1;
+  }
+  .text-color-primary {
+    color: blue;
+  }
+</style>
