1bec0ae99465436a85baaca3ae05623a50d77f29
yoshiji <yfbiology2@gmail.com>
2019-05-13 16:07:47 +0900

    server setup

---
+++ server/config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  DB_URI: 'mongodb+srv://Yoshiji:Pilot25234@fullstack-vue-graphql-ccfms.mongodb.net/test?retryWrites=true',
+  SESSION_SECRET: 'sd88d!+asd7jbhxbhj',
+  JWT_SECRET: 'JASD_skdak+12e'
+}
