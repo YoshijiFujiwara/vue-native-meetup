3b5303b9701783c00d6b1a287dfb9b4b5817730a
yoshiji <yfbiology2@gmail.com>
2019-05-14 20:19:36 +0900

    login and register switchNavigator

--- src/navigation.vue
+++ src/navigation.vue
@@ -7,6 +7,8 @@
 <script>
   import HomeScreen from '@/screens/HomeScreen';
   import MeetupDetailScreen from '@/screens/MeetupDetailScreen';
+  import LoginScreen from '@/screens/LoginScreen';
+  import RegisterScreen from '@/screens/RegisterScreen';
   import Screen1 from '@/screens/Screen1';
   import Screen2 from '@/screens/Screen2';
   import Screen3 from '@/screens/Screen3';
@@ -15,6 +17,7 @@
   import { createStackNavigator,
             createBottomTabNavigator,
             createDrawerNavigator,
+            createSwitchNavigator,
             createAppContainer } from 'react-navigation';
 
   const HomeStack = createStackNavigator(
@@ -27,6 +30,11 @@
       initialRouteName: 'Home'
     }
   );
+
+  const AuthStack = createStackNavigator({
+    Login: LoginScreen,
+    Register: RegisterScreen
+  });
 
   const SomeOtherStack = createStackNavigator({
     Screen2
@@ -56,7 +64,10 @@
     Other: DrawerNavigation
   });
 
-  const AppNavigation = createAppContainer(TabNavigation)
+  const AppNavigation = createAppContainer(createSwitchNavigator({
+    auth: AuthStack,
+    tabs: TabNavigation,
+  }))
 
   export default {
     components: {
