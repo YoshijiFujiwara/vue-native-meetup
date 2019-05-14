47e096687b474c1724b7f292eec5705da959842d
yoshiji <yfbiology2@gmail.com>
2019-05-14 15:41:28 +0900

    meetup detail info

---
+++ src/components/MeetupDetailInfo.vue
@@ -0,0 +1,77 @@
+<template>
+  <view>
+    <nb-card-item bordered>
+      <nb-body>
+        <!-- Label Styles -->
+        <nb-text :style="styles.label">
+          日付
+        </nb-text>
+        <nb-text>
+          {{meetup.startDate}}
+        </nb-text>
+        <!-- Label Styles -->
+        <nb-text :style="styles.label">
+          開始時刻
+        </nb-text>
+        <nb-text>
+          {{meetup.timeFrom}}
+        </nb-text>
+        <!-- Label Styles -->
+        <nb-text :style="styles.label">
+          終了時刻
+        </nb-text>
+        <nb-text>
+          {{meetup.timeTo}}
+        </nb-text>
+        <!-- Label Styles -->
+        <nb-text :style="styles.label">
+          カテゴリー
+        </nb-text>
+        <nb-text>
+          {{meetup.category.text}}
+        </nb-text>
+        <!-- Label Styles -->
+        <nb-text :style="styles.label">
+          詳細
+        </nb-text>
+        <!-- Label Styles -->
+        <!-- short info -->
+        <nb-text>
+          {{meetup.description}}
+        </nb-text>
+      </nb-body>
+    </nb-card-item>
+    <nb-card-item header bordered>
+      <nb-text>概要</nb-text>
+    </nb-card-item>
+    <nb-card-item bordered>
+      <nb-body>
+        <!-- description -->
+        <nb-text>
+          NativeBase is a free and open source framework that enable developers to build high-quality mobile apps using React Native iOS and Android apps with a fusion of ES6.
+        </nb-text>
+      </nb-body>
+    </nb-card-item>
+  </view>
+</template>
+
+<script>
+  import styles from '@/styles';
+  export default {
+    props: {
+      meetup: {
+        type: Object,
+        required: true
+      }
+    },
+    data() {
+      return {
+        styles
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- src/screens/MeetupDetailScreen.vue
+++ src/screens/MeetupDetailScreen.vue
@@ -1,80 +1,36 @@
 <template>
-  <nb-container>
-    <!-- meetup title -->
-    <nb-h1 :style="[styles.headerOne, styles.container]">Hiking in Almada</nb-h1>
-    <!-- meetup creator avatar  -->
-    <nb-thumbnail />
+  <!-- 読み込まれてから、表示する -->
+  <nb-container v-if="isMeetupLoaded">
+    <view :style="styles.container">
+      <!-- meetup title -->
+      <nb-h1 :style="[styles.headerOne, styles.container]">{{meetup.title}}</nb-h1>
+      <!-- meetup creator avatar  -->
+      <nb-thumbnail :source="{uri: meetupCreator.avatar}" />
+    </view>
     <!-- apply styles label and padding left -->
     <!-- meetup creator name -->
     <nb-text :style="[styles.label, { paddingLeft: 20 }]">
-      by Filip Jerga
+      by {{meetupCreator.name}}
     </nb-text>
     <nb-content>
       <nb-card>
         <nb-card-item header bordered>
-          <nb-text>Details</nb-text>
+          <nb-text>詳細</nb-text>
         </nb-card-item>
-        <nb-card-item bordered>
-          <nb-body>
-            <!-- Label Styles -->
-            <nb-text :style="styles.label">
-              Date
-            </nb-text>
-            <nb-text>
-              14th Feb 2018
-            </nb-text>
-            <!-- Label Styles -->
-            <nb-text :style="styles.label">
-              From
-            </nb-text>
-            <nb-text>
-              14:00
-            </nb-text>
-            <!-- Label Styles -->
-            <nb-text :style="styles.label">
-              To
-            </nb-text>
-            <nb-text>
-              13:00
-            </nb-text>
-            <!-- Label Styles -->
-            <nb-text :style="styles.label">
-              Category
-            </nb-text>
-            <nb-text>
-              Music
-            </nb-text>
-            <!-- Label Styles -->
-            <nb-text :style="styles.label">
-              Info
-            </nb-text>
-            <!-- Label Styles -->
-            <!-- short info -->
-            <nb-text>
-              Some nice short info
-            </nb-text>
-          </nb-body>
-        </nb-card-item>
-        <nb-card-item header bordered>
-          <nb-text>Description</nb-text>
-        </nb-card-item>
-        <nb-card-item bordered>
-          <nb-body>
-            <!-- description -->
-            <nb-text>
-              NativeBase is a free and open source framework that enable developers to build high-quality mobile apps using React Native iOS and Android apps with a fusion of ES6.
-            </nb-text>
-          </nb-body>
-        </nb-card-item>
+        <MeetupDetailInfo :meetup="meetup" />
       </nb-card>
     </nb-content>
   </nb-container>
 </template>
 
 <script>
+  import MeetupDetailInfo from '@/components/MeetupDetailInfo';
   import styles from '@/styles';
 
   export default {
+    components: {
+      MeetupDetailInfo
+    },
     props: {
       navigation: {
         type: Object,
@@ -88,7 +44,13 @@
     computed: {
       meetup() {
         return this.$store.state.meetups.item;
-      }
+      },
+      isMeetupLoaded() {
+        return Object.keys(this.meetup).length > 0;
+      },
+      meetupCreator() {
+        return this.meetup.meetupCreator || {}
+      },
     },
     created() {
       // 画面遷移してきたときに、変数を渡す
